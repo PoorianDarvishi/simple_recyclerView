ea844172893859581e381ba0cb3e6dbedb8e2984
pooria-darvishi <pooria.darvishi.82@gmail.com>
2023-03-10 11:47:37 +0330

    add recyclerView

--- app/src/main/java/com/example/recyclerview/MainFragment.kt
+++ app/src/main/java/com/example/recyclerview/MainFragment.kt
@@ -1,16 +1,28 @@
 package com.example.recyclerview
 
+import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview.databinding.FragmentMainBinding
 
 class MainFragment: Fragment(R.layout.fragment_main) {
     lateinit var binding : FragmentMainBinding
-    val mainViewModel :
+    val mainViewModel : MainViewModel by viewModels()
+    lateinit var adapter: Adapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
-
+        mainViewModel.setListCity(ListCity.listCity)
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        mainViewModel.listCityLiveData.observe(viewLifecycleOwner
+        ) { t ->
+            adapter =
+                Adapter(t!!)
+        }
     }
 }
